becb66b35adf0a2d7c1ca2e95cc79a5ae9d32d7a
Callum Ingley <callum.ingley@gmail.com>
2015-03-20 11:08:39 +0800

    last version of routes and url_for

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
+@app.route('/profile/<username>')
+def show_user_profile(username):
+    return 'User: {}'.format(username)
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def show_url_for():
+    return url_for('show_user_profile', username='callum') 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
